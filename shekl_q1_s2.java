80eb11cf4ce7d427087969332412c2e404187432
pooyastod <100297480+pooyastod@users.noreply.github.com>
2022-03-02 15:40:23 +0330

    Create shekl_q1_s2.java

---
+++ shekl_q1_s2.java
@@ -0,0 +1,10 @@
+public class shekl_q1_s2 {
+    double tool;
+    double arz;
+    public void masahat(){
+        System.out.println(tool * arz);
+    }
+    public void mohit(){
+        System.out.println(2*(tool + arz));
+    }
+}
